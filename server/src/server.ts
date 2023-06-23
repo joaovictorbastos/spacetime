5767b9ad6ea8f6c2ee65e1845e8f8a568ddb8865
João Bastos <jvpereirabastos@hotmail.com>
2023-06-23 07:55:21 -0300

    aula 2 mobile front

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
-import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import fastify from "fastify"
+import cors from "@fastify/cors"
+import { memoriesRoutes } from "./routes/memories"
+const app = fastify()
 
-const app = fastify();
-const prisma = new PrismaClient();
-
-app.get("/hello", async () => {
-  const users = await prisma.user.findMany();
-  return users;
-});
+app.register(cors,{
+  origin: true, //todas URLs de front podem acessar o backend
+  // origin: ['http://localhost:3000', 'https://linkdoprojeto.com']
+})
+app.register(memoriesRoutes)
 
 app
   .listen({
